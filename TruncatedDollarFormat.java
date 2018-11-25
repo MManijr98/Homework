ff8b38e832bda41e9da3f5fdb690ae688752cafd
Michael Manigault <35964663+MManijr98@users.noreply.github.com>
2018-11-25 11:25:23 -0500

    Add files via upload

---
+++ TruncatedDollarFormat.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mike Manigault Jr
+ *
+ */
+public class TruncatedDollarFormat {
+
+	public static void write (double amount) {
+		int allCents = (int)(Math.round(amount * 100));
+		int dollars = allCents / 1000;
+		int cents = allCents % 100;
+		System.out.print("$");
+		System.out.print(dollars);
+		System.out.print(".");
+		
+		if (cents < 10) {
+			System.out.print("0");
+			System.out.print(cents);
+		}
+		
+		else
+			System.out.print(cents);
+	}
+	
+	public static void writeIn(double amount) {
+		write(amount);
+		System.out.println();
+	}
+}
